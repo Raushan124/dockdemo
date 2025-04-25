f819546c3a6e16b4fb9a23b7659e3bb460f7aa9d
Raushan124 <90439056+Raushan124@users.noreply.github.com>
2025-04-26 01:23:29 +0530

    Update Docktest.java

--- src/main/java/com/dock/controller/Docktest.java
+++ src/main/java/com/dock/controller/Docktest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Docktest {
-	@GetMapping
+	@GetMapping("/all")
 	public String run() {
 		return "Hello World";
 	}
